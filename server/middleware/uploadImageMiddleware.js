1c714f3b6670800ee9b4839030071e3aad3d3a8c
Clawwo <genzz760@gmail.com>
2025-09-11 13:59:23 +0700

    feat: menambahkan filter untuk upload gambar

--- server/middleware/uploadImageMiddleware.js
+++ server/middleware/uploadImageMiddleware.js
@@ -1,6 +1,25 @@
+// uploadMiddleware.js
 import multer from "multer";
 
+// simpan di memory
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+
+// maksimal 2MB
+const MAX_SIZE = 2 * 1024 * 1024;
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true); 
+  } else {
+    cb(new Error("Format file harus JPG atau PNG"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: MAX_SIZE },
+  fileFilter,
+});
 
 export default upload;
